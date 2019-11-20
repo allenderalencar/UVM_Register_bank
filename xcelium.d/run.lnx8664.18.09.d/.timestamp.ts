78571330867a19e6c3a34f723ad641d0e7b646b4
Allender Vilar de Alencar <allender.alencar@embedded.ufcg.edu.br>
2019-11-20 19:59:14 -0300

    correcting

--- xcelium.d/run.lnx8664.18.09.d/.timestamp.ts
+++
@@ -1,7 +0,0 @@
-1574183756 /home/allender/UVN]/UVM_Register_bank/rtl/datapath.sv
-1574183756 /home/allender/UVN]/UVM_Register_bank/tb/top.sv
-1574285394 /home/allender/UVN]/UVM_Register_bank/tb/pkg.sv
-1574183756 /home/allender/UVN]/UVM_Register_bank/tb/interface_if.sv
-1574183756 /home/allender/UVN]/UVM_Register_bank/rtl/mux.sv
-1574183756 /home/allender/UVN]/UVM_Register_bank/rtl/rb.sv
-1574183756 /home/allender/UVN]/UVM_Register_bank/rtl/ula.sv
