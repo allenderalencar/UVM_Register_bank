4dc565634488a5bca4641ae482a8272056aa7407
Allender Vilar de Alencar <allender.alencar@embedded.ufcg.edu.br>
2019-11-22 11:44:33 -0300

    commiting remove

--- xcelium.d/run.lnx8664.18.09.d/.timestamp.ts
+++
@@ -1,7 +0,0 @@
-1574432993 /home/allender/muda/UVM_Register_bank/tb/interface_if.sv
-1574433005 /home/allender/muda/UVM_Register_bank/tb/top.sv
-1574427283 /home/allender/muda/UVM_Register_bank/rtl/datapath.sv
-1574429648 /home/allender/muda/UVM_Register_bank/tb/bank_pkg.sv
-1574427283 /home/allender/muda/UVM_Register_bank/rtl/rb.sv
-1574427283 /home/allender/muda/UVM_Register_bank/rtl/mux.sv
-1574427283 /home/allender/muda/UVM_Register_bank/rtl/ula.sv
